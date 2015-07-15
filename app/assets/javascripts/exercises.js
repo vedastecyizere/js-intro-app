dc2d2add87bdc581752bc8dd9621563502ef01fb
Jay Wengrow <jaywngrw@gmail.com>
2015-07-14 20:51:13 -0500

    solutions from emperor penguins

--- app/assets/javascripts/exercises.js
+++ app/assets/javascripts/exercises.js
@@ -14,6 +14,11 @@
 // 1
 // BLASTOFF!
 
+// for(var i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+// console.log("BLASTOFF!");
+
 // Hint: Use a for loop!
 
 // // Exercise #2:
@@ -22,11 +27,25 @@
 
 // Write some code that prints all the numbers in the 'numbers' array to the console.
 
+// for(var i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
 // Hint: Use a for loop!
 
 // // Exercise #3:
 
+// var numbers = [5, 23, 98, 234, 1, 0, 3, 177];
+
 // Write code that finds the average of all the numbers in the 'numbers' array.
+
+// var sum = 0;
+
+// for (var i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// console.log(sum / numbers.length);
+
 // Hint: Use a for loop!
 
 // // Exercise #4:
@@ -34,6 +53,19 @@
 // Implement FizzBuzz in JavaScript. Write a program that prints the integers from 1 to 100.
 // But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". 
 // For numbers which are multiples of both three and five print "FizzBuzz".
+
+// for(var i = 1; i <= 100; i++) {
+//   if (i % 15 === 0) {
+//     console.log("FIZZBUZZ");
+//   } else if (i % 5 === 0) {
+//     console.log("BUZZ");
+//   } else if (i % 3 === 0) {
+//     console.log("FIZZ")
+//   } else {
+//     console.log(i);
+//   }
+// }
+
 
 // Hint: See other hints!
 
@@ -43,17 +75,59 @@
 // For example:
 // var guess = prompt("Guess a number");
 
+// var answer = Math.floor((Math.random() * 100) + 1);
+
+// for (var i = 0; i < 10; i++) {
+//   var guess = parseInt(prompt("Guess a number:"));
+//   if (guess < answer) {
+//     console.log("Guess higher!");
+//   } else if (guess > answer) {
+//     console.log("Guess lower!");
+//   } else {
+//     console.log("You win!");
+//     break;
+//   }
+// }
+
 // // Exercise #6:
 
-// Create a function that accepts two numbers as parameters and returns their sum.
+// Create a function that accepts two numbers as parameters and returns their product.
 
 // // Exercise #7:
 
 // Create a function that accepts an array as an argument and returns the average. (Basically the same as Exercise #3, but wrap that code in a function and demonstrate its usage.)
 
+// var user_numbers = [];
+
+// for(var i = 0; i < 5; i++) {
+//   user_numbers.push(parseInt(prompt("Enter a number")));
+// }
+
+// function average(numbers) {
+//   var sum = 0;
+
+//   for (var i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   return (sum / numbers.length);
+// }
+
+// console.log(average(user_numbers));
+
 // // Exercise #8:
 
 // Create an object that represents a particular book, having attributes of title, author, isbn, pages, and year of publication.
+
+// var book = {
+//   title: "Cat in the Hat",
+//   author: "Dr. Seuss",
+//   isbn: 6392770,
+//   pages: 32,
+//   yearOfPublication: 1962,
+//   printInfo: function() {
+//     console.log(this.title + ", " + this.author + ", " + this.isbn + ", " + this.pages + ", " + this.yearOfPublication)
+//   }
+// };
 
 // // Exercise #9:
 
