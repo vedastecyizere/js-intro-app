06394ba0ec8702a16e50d34fecfe6ca4d64889de
Vedaste Cyizere <revedaste@gmail.com>
2017-02-23 10:31:09 +0200

    using three box game

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,1 +1,12 @@
+var jackhammerClicked = 1;
+
+function jackhammer(){
+  if (jackhammerClicked === 5){
+    alert("you won!!!");
+  } else 
+    console.log("You are on the right track");
+    jackhammerClicked++;
+}
+
+
 
--- app/assets/javascripts/three.js
+++ app/assets/javascripts/three.js
@@ -1,1 +1,10 @@
+var winningBox = Math.floor((Math.random() * 3) + 1);
 
+function three(times){
+  if (times === 3); {
+    alert("you win");
+  } else {
+    console.log("Try again");
+  }
+    
+}
